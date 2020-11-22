4a526bd5e6253f07a026da987cc1d9764f9d1928
kensevv <kensev2000@gmail.com>
2020-11-22 12:04:20 +0200

    Task 7 improvement

--- exercise 4/task 7.js
+++ exercise 4/task 7.js
@@ -1,7 +1,7 @@
 function switchRows(){
-    var row1 = document.getElementsByTagName("tr")[1];
-    var row2 = document.getElementsByTagName("tr")[2];
+    var row1 = document.getElementsByTagName("tr")[1].innerText;
+    var row2 = document.getElementsByTagName("tr")[2].innerText;
 
-    document.getElementsByTagName("tr")[1].innerHTML = row2; // :D
-    document.getElementsByTagName("tr")[2].innerHTML = row1; // :D
+    document.getElementsByTagName("tr")[1].innerHTML =`${row2}`; // :D
+    document.getElementsByTagName("tr")[2].innerHTML = `${row1}`; // :D
 }
